3d59e3477732db7ec521be803e33789e794c6648
Scott Kolby <skolby42@gmail.com>
2020-07-01 18:12:59 -0700

    Update gitignore to only exclude subfolders

---
+++ Terminal Hacker/Assets/WM2000/Hacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private static void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Where would you like to hack into?\n");
+        Terminal.WriteLine("1. School");
+        Terminal.WriteLine("2. Police Station");
+        Terminal.WriteLine("3. NSA\n");
+        Terminal.WriteLine("Enter your selection:");
+    }
+}
