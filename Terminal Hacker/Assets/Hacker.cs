233406d7725514cd1e7ceb52f3d8fc34c5028e16
Scott Kolby <skolby42@gmail.com>
2020-07-02 09:14:29 -0700

    Add input handling

--- Terminal Hacker/Assets/Hacker.cs
+++ Terminal Hacker/Assets/Hacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,23 +11,42 @@ public class Hacker : MonoBehaviour
         ShowMainMenu();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private static void ShowMainMenu()
+    private void ShowMainMenu()
     {
         Terminal.ClearScreen();
-
-        string Name = "Scott";
-        Terminal.WriteLine($"Hello {Name}");
-
-        Terminal.WriteLine("Where would you like to hack into?\n");
+        Terminal.WriteLine("What would you like to hack into?\n");
         Terminal.WriteLine("1. School");
         Terminal.WriteLine("2. Police Station");
         Terminal.WriteLine("3. NSA\n");
         Terminal.WriteLine("Enter your selection:");
+    }
+
+    private void OnUserInput(string input)
+    {
+        if (string.IsNullOrEmpty(input)) return;
+
+        switch (input)
+        {
+            case "1":
+                Terminal.WriteLine("Hacking School");
+                break;
+            case "2":
+                Terminal.WriteLine("Hacking Police Station");
+                break;
+            case "3":
+                Terminal.WriteLine("Hacking NSA");
+                break;
+            case "007":
+                Terminal.WriteLine("Please make a selection, Mr. Bond");
+                break;
+            case "menu":
+                {
+                    ShowMainMenu();
+                    break;
+                }
+            default:
+                Terminal.WriteLine("Please enter a valid input.");
+                break;
+        }
     }
 }
