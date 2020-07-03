e2f684f7dadcb85cd872b83fb1ab1c309ad381f4
Scott Kolby <skolby42@gmail.com>
2020-07-02 17:17:22 -0700

    Organize assets.  Add simple tag collision handling.

--- Thrust Vector/Assets/Rocket.cs
+++ Thrust Vector/Assets/Rocket.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;
-    AudioSource audioSource;
+    [SerializeField]
+    private float mainThrust = 750f;
+    [SerializeField]
+    private float rcsThrust = 250f;
+    
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;  
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +23,29 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly": // OK
+                break;
+            case "Fuel":  // Add fuel
+                break;
+            default:  // Kill player
+                break;
+        }
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            float thrustThisFrame = mainThrust * Time.deltaTime;
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
 
             if (!audioSource.isPlaying)
                 audioSource.Play();
@@ -34,14 +54,23 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;  // Take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false;  // Resume physics control
     }
 }
