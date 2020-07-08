7dc32700b7ff1b73b731f53e2cf350b737aeabe3
Scott Kolby <skolby42@gmail.com>
2020-07-08 16:24:47 -0700

    Initial commit

---
+++ Planetary Defender/Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 2f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadFirstLevel", loadDelay);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
