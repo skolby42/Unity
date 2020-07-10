7671b5716b9dc29a7897474c891e99097d8f50b8
Scott Kolby <skolby42@gmail.com>
2020-07-10 12:02:05 -0700

    Add enemy timeline

---
+++ Planetary Defender/Assets/Editor/TerrainCustomEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RaiseTerrainHeightmap))]
+public class TerrainCustomEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        RaiseTerrainHeightmap RaiseTerrain = (RaiseTerrainHeightmap)target;
+        if (GUILayout.Button("Raise Terrain Heightmap"))
+        { RaiseTerrain.RaiseHeights(); }
+    }
+}
---
+++ Planetary Defender/Assets/Scripts/RaiseTerrainHeightmap.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+public class RaiseTerrainHeightmap : MonoBehaviour
+{
+
+    public Terrain myTerrain;
+    private TerrainData terrainData;
+    private int heightmapWidth;
+    private int heightmapHeight;
+    private float[,] heightmapData = new float[0, 0];
+
+    public float raiseHeightInUnits = 20.0f;
+
+
+
+    public void RaiseHeights()
+    {
+        // - GetTerrainData -
+
+        if (!myTerrain)
+        {
+            //myTerrain = Terrain.activeTerrain;
+            Debug.LogError(gameObject.name + " has no terrain assigned in the inspector");
+        }
+
+        terrainData = myTerrain.terrainData;
+        heightmapWidth = myTerrain.terrainData.heightmapResolution;
+        heightmapHeight = myTerrain.terrainData.heightmapResolution;
+
+        // --
+
+        // store old heightmap data
+        heightmapData = terrainData.GetHeights(0, 0, heightmapWidth, heightmapHeight);
+
+        float terrainHeight = terrainData.size.y;
+
+        // --
+
+        var y = 0;
+        var x = 0;
+
+        // raise heights
+        for (y = 0; y < heightmapHeight; y++)
+        {
+            for (x = 0; x < heightmapWidth; x++)
+            {
+                float newHeight = Mathf.Clamp01(heightmapData[y, x] + (raiseHeightInUnits / terrainHeight));
+
+                heightmapData[y, x] = newHeight;
+            }
+        }
+
+        terrainData.SetHeights(0, 0, heightmapData);
+
+        Debug.Log("RaiseHeights() completed");
+    }
+
+
+}
