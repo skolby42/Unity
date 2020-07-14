40305105decc91e7d588a47558c72add48747c8d
Scott Kolby <skolby42@gmail.com>
2020-07-14 09:02:48 -0700

    Initial commit

---
+++ Realm Rush/Assets/EditorSnap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = 0f;  // Disable snap in Y axis
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = snapPos;
+    }
+}
