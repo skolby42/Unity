abf7c8577e03e47f3a00e32b252bec63edf9f715
Scott Kolby <skolby42@gmail.com>
2020-07-14 09:53:04 -0700

    Snap cube to grid position and update text

---
+++ Realm Rush/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10;
+
+    TextMesh textMesh;
+
+    void Update()
+    {
+        SetSnapPosition();
+        UpdateText();
+    }
+
+    private void SetSnapPosition()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = 0f;  // Disable snap in Y axis
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = snapPos;
+    }
+
+    private void UpdateText()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+
+        float gridX = transform.position.x / gridSize;
+        float gridZ = transform.position.z / gridSize;
+
+        textMesh.text = $"{gridX},{gridZ}";
+    }
+}
