258bdf21758089ed9ac397e34e215ecc2f877557
Scott Kolby <skolby42@gmail.com>
2020-07-15 17:53:27 -0700

    Add enemy spawner

---
+++ Realm Rush/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] EnemyMovement enemyPrefab = null;
+    [SerializeField] float secondsBetweenSpawns = 10f;
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyRepeating());
+    }
+
+    private IEnumerator SpawnEnemyRepeating()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+            Instantiate(enemyPrefab, transform, true);
+        }
+    }
+}
