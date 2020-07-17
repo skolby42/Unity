b7fd79ff8288af999f55bc79f8790791d5e8ff21
Scott Kolby <skolby42@gmail.com>
2020-07-17 12:30:08 -0700

    Initial commit

---
+++ Battle Zone/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target = null;
+    [SerializeField] float chaseRange = 5f;
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        MoveToTarget();
+    }
+
+    private void MoveToTarget()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+}
