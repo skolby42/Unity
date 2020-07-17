d3de808554cdf8e4a1055692833e90eb9311e8f5
Scott Kolby <skolby42@gmail.com>
2020-07-17 16:12:35 -0700

    Rename project

---
+++ Invasion Force/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target = null;
+    [SerializeField] float chaseRange = 5f;
+    
+    NavMeshAgent navMeshAgent;
+    bool isProvoked;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (Vector3.Distance(target.position, transform.position) <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (Vector3.Distance(target.position, transform.position) >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        print("Attacking target");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
