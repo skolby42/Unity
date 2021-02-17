0e8dfd673167fcaedf7a96fecde7d3453cc48be4
Scott Kolby <skolby42@gmail.com>
2021-02-17 15:36:13 -0800

    Add basic level & flashlight

---
+++ Zombie Hunter/Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 1.0f;
+    [SerializeField] float minAngle = 1.0f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    private void DecreaseLightAngle()
+    {
+        var angle = myLight.spotAngle - Time.deltaTime * angleDecay;
+        Mathf.Clamp(angle, minAngle, myLight.spotAngle);
+        myLight.spotAngle = angle;
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        var intensity = myLight.intensity - Time.deltaTime * lightDecay;
+        Mathf.Clamp(intensity, 0f, myLight.intensity);
+        myLight.intensity = intensity;
+    }
+}
